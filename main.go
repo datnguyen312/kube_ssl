59b221dbf15381f26026ccee20e71f708e488fa0
thluong <thluong@axon.com>
2018-09-11 13:15:51 +0700

    add log + error handle

--- main.go
+++ main.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	// "flag"
-	"fmt"
+    "io"
+    "log"
+	// "fmt"
 	"time"
+	"net/http"
 	"io/ioutil"
 	"encoding/base64"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	certificatesv1beta1 "k8s.io/client-go/kubernetes/typed/certificates/v1beta1"
-	v1beta1 "k8s.io/api/certificates/v1beta1"
 	rest "k8s.io/client-go/rest"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1beta1 "k8s.io/api/certificates/v1beta1"
+	certificatesv1beta1 "k8s.io/client-go/kubernetes/typed/certificates/v1beta1"
 )
+
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+func Init(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    Trace = log.New(traceHandle,
+        "TRACE: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Info = log.New(infoHandle,
+        "INFO: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Warning = log.New(warningHandle,
+        "WARNING: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Error = log.New(errorHandle,
+        "ERROR: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+}
+
 
 func OutClusterConfig() *rest.Config {
 	certData, _ := ioutil.ReadFile("/etc/kubernetes/pki/apiserver-kubelet-client.crt")
@@ -64,7 +97,7 @@ var Config *rest.Config
 // 	}
 // }
 
-func csrApprover(r clientQuery) {
+func csrApprover(r clientQuery) error {
 	// Just sleep 1 sec to make sure the object has been sync
 	time.Sleep(time.Duration(1) * time.Second)
 	certConfig, err := certificatesv1beta1.NewForConfig(Config)
@@ -77,44 +110,50 @@ func csrApprover(r clientQuery) {
 
 	if errRequest != nil { panic(errRequest) }
 	_, err = certConfig.CertificateSigningRequests().UpdateApproval(certRequest)
-	if err != nil { panic(err) }
+	if err != nil { 
+		Error.Println(err) 
+		return err
+	}
+	return err
 }
 
-func csrRequester(r clientQuery) {
+func csrRequester(r clientQuery) error {
 	var decoded []byte
 	certConfig, err := certificatesv1beta1.NewForConfig(Config)
 	if err != nil { panic(err) }	
 
 	decoded, err = base64.StdEncoding.DecodeString(r.CsrData)
 	if err != nil {
-		panic(err)
+		Error.Println(err)
+		return err
 	} else {
-		fmt.Println(string(decoded))
-	}
-
-	var certRequest = &v1beta1.CertificateSigningRequest{
-		ObjectMeta: v1.ObjectMeta{
-			Name: r.Nodeip,
-		},
-		Spec: v1beta1.CertificateSigningRequestSpec{
-			Usages: []v1beta1.KeyUsage{
-				v1beta1.UsageServerAuth,
-				v1beta1.UsageClientAuth,
+		var certRequest = &v1beta1.CertificateSigningRequest{
+			ObjectMeta: v1.ObjectMeta{
+				Name: r.Nodeip,
 			},
-			Request: decoded,
-		},
-	}
+			Spec: v1beta1.CertificateSigningRequestSpec{
+				Usages: []v1beta1.KeyUsage{
+					v1beta1.UsageServerAuth,
+					v1beta1.UsageClientAuth,
+				},
+				Request: decoded,
+			},
+		}
 
-	_, err = certConfig.CertificateSigningRequests().Create(certRequest)
-	if err != nil { panic(err) }	
+		_, err = certConfig.CertificateSigningRequests().Create(certRequest)
+		if err != nil { 
+			Error.Println(err) 
+			return err
+		}	
+	}
+	return err
 }
 
 func crtDownload(r clientQuery) string {
 	// Just sleep 1 sec to make sure the object has been sync
 	time.Sleep(time.Duration(1) * time.Second)
 	certConfig, err := certificatesv1beta1.NewForConfig(Config)
-	if err != nil { panic(err) }	
-
+	if err != nil { panic(err) }
 	certRequest, errRequest := certConfig.CertificateSigningRequests().Get(r.Nodeip, v1.GetOptions{})
 	if errRequest != nil { panic(errRequest) }
 
@@ -125,8 +164,12 @@ func request(c *gin.Context) {
 	var r clientQuery
 	c.BindJSON(&r)
 	if r.Nodename != "" && r.Nodeip != "" && r.CsrData != ""{
-		csrRequester(r)
-		csrApprover(r)
+		if err := csrRequester(r); err != nil {
+			c.String(500, err.Error())
+		}
+		if err := csrApprover(r); err != nil {
+			c.String(500, err.Error())	
+		}
 		certCRT := crtDownload(r)
 		c.String(200, certCRT)
 	} else {
