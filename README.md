7631a33321e300e72a23484cbd7fa1afde184811
thluong <thluong@axon.com>
2018-09-11 10:57:20 +0700

    first init

---
+++ README.md
@@ -0,0 +1,50 @@
+# Kubernetes SSL Approver
+
+# Diagram
+
+# Reason
+- deploy kubernetes via kubeadm when using ssl mode, the ssl cert must be created and configured with IPSAN to avoid in breaking tls verification (metrics-server etc.)
+
+# Target
+- new node will make a csr request with IPSANs or custom cert etc... and approved by kubernetes CA
+
+# How to
+- This api will run on master, and uses apiserver-kubelet-client cert/key to authentication/authorization with api server
+- On the new node worker with cfssl installed
+  + 
+  ```
+cat <<EOF | cfssl genkey - | cfssljson -bare server
+{
+  "hosts": [
+    "192.168.33.13"
+  ],
+  "CN": "192.168.33.13",
+  "key": {
+    "algo": "ecdsa",
+    "size": 256
+  }
+}
+EOF
+  ```
+  +
+  ``` 
+  cat > server.json << EOF 
+{
+	"nodename": "192.168.33.13",
+	"nodeip": "192.168.33.13",
+	"csrdata": $(cat server.csr) |base64 | tr -d "\n"
+}
+EOF
+  ```
+  + curl  -H "Content-Type: application/json" -X POST --data @server.json http://127.0.0.1:9999/v1/request > server.crt
+
+- Recheck the cert:
+  + openssl x509 -in server.crt -text -noout
+
+- Check the certificate approved or not
+  + kubectl get csr
+
+# Build
+- install glide
+- glide up
+- CGO_ENABLED=0 env GOOS=linux go build
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	// "flag"
+	"fmt"
+	"time"
+	"io/ioutil"
+	"encoding/base64"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	certificatesv1beta1 "k8s.io/client-go/kubernetes/typed/certificates/v1beta1"
+	v1beta1 "k8s.io/api/certificates/v1beta1"
+	rest "k8s.io/client-go/rest"
+)
+
+func OutClusterConfig() *rest.Config {
+	certData, _ := ioutil.ReadFile("/etc/kubernetes/pki/apiserver-kubelet-client.crt")
+
+	keyData, _ := ioutil.ReadFile("/etc/kubernetes/pki/apiserver-kubelet-client.key")
+
+	config := &rest.Config{
+		Host:    "https://127.0.0.1:6443",
+		APIPath: "/api",
+		TLSClientConfig: rest.TLSClientConfig{
+			Insecure: true,
+			CertFile: "/etc/kubernetes/pki/apiserver-kubelet-client.crt",
+			KeyFile:  "/etc/kubernetes/pki/apiserver-kubelet-client.key",
+			CertData: certData,
+			KeyData:  keyData,
+		},
+	}
+
+	return config
+}
+
+type clientQuery struct {
+	Nodename string `json:"nodename"`
+	Nodeip 	 string `json:"nodeip"`
+	CsrData  string `json:"csrdata"`
+}
+
+var Config *rest.Config
+
+// func csrApprover() {
+// 	certConfig, err := certificatesv1beta1.NewForConfig(Config)
+// 	if err != nil { panic(err) }	
+
+// 	eventChan, errEvent := certConfig.CertificateSigningRequests().Watch(v1.ListOptions{})
+// 	if errEvent != nil { panic(errEvent) }
+
+// 	ch := eventChan.ResultChan()
+// 	for {
+// 		msg := <-ch
+// 		certRequest := msg.Object.(*v1beta1.CertificateSigningRequest)
+// 		fmt.Println(certRequest.ObjectMeta.Name)
+// 		if certRequest.Status.Conditions == nil {
+// 			certRequest.Status.Conditions = append(certRequest.Status.Conditions, v1beta1.CertificateSigningRequestCondition{
+// 				Type: v1beta1.CertificateApproved,
+// 			})
+// 			_, err := certConfig.CertificateSigningRequests().UpdateApproval(certRequest)
+// 			if err != nil { panic(err) }
+// 		}
+// 	}
+// }
+
+func csrApprover(r clientQuery) {
+	// Just sleep 1 sec to make sure the object has been sync
+	time.Sleep(time.Duration(1) * time.Second)
+	certConfig, err := certificatesv1beta1.NewForConfig(Config)
+	if err != nil { panic(err) }	
+
+	certRequest, errRequest := certConfig.CertificateSigningRequests().Get(r.Nodeip, v1.GetOptions{})
+	certRequest.Status.Conditions = append(certRequest.Status.Conditions, v1beta1.CertificateSigningRequestCondition{
+		Type: v1beta1.CertificateApproved, // Approved cert
+	})
+
+	if errRequest != nil { panic(errRequest) }
+	_, err = certConfig.CertificateSigningRequests().UpdateApproval(certRequest)
+	if err != nil { panic(err) }
+}
+
+func csrRequester(r clientQuery) {
+	var decoded []byte
+	certConfig, err := certificatesv1beta1.NewForConfig(Config)
+	if err != nil { panic(err) }	
+
+	decoded, err = base64.StdEncoding.DecodeString(r.CsrData)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(string(decoded))
+	}
+
+	var certRequest = &v1beta1.CertificateSigningRequest{
+		ObjectMeta: v1.ObjectMeta{
+			Name: r.Nodeip,
+		},
+		Spec: v1beta1.CertificateSigningRequestSpec{
+			Usages: []v1beta1.KeyUsage{
+				v1beta1.UsageServerAuth,
+				v1beta1.UsageClientAuth,
+			},
+			Request: decoded,
+		},
+	}
+
+	_, err = certConfig.CertificateSigningRequests().Create(certRequest)
+	if err != nil { panic(err) }	
+}
+
+func crtDownload(r clientQuery) string {
+	// Just sleep 1 sec to make sure the object has been sync
+	time.Sleep(time.Duration(1) * time.Second)
+	certConfig, err := certificatesv1beta1.NewForConfig(Config)
+	if err != nil { panic(err) }	
+
+	certRequest, errRequest := certConfig.CertificateSigningRequests().Get(r.Nodeip, v1.GetOptions{})
+	if errRequest != nil { panic(errRequest) }
+
+	return string(certRequest.Status.Certificate)
+}
+
+func request(c *gin.Context) {
+	var r clientQuery
+	c.BindJSON(&r)
+	if r.Nodename != "" && r.Nodeip != "" && r.CsrData != ""{
+		csrRequester(r)
+		csrApprover(r)
+		certCRT := crtDownload(r)
+		c.String(200, certCRT)
+	} else {
+		c.String(404, "Missing nodename or nodeip or csrdata")		
+	}
+}
+
+func main() {
+	Config = OutClusterConfig()
+	// go csrApprover()
+
+	r := gin.Default()
+	r.POST("v1/request", request)
+	s := &http.Server{
+		Addr:           ":9999",
+		Handler:        r,
+		ReadTimeout:    120 * time.Second,
+		WriteTimeout:   120 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
